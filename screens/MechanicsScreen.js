e52671876253f65ad5fea6d6a7abe3e0b528ed57
koomekevo <koome.kevo@gurucrafts.net>
2023-07-29 17:20:24 +0300

    Display fetched mechanics on the map

--- screens/MechanicsScreen.js
+++ screens/MechanicsScreen.js
@@ -4,6 +4,7 @@ import { View } from 'react-native';
 import { Container, Content, Header, Icon, Left, Right, Title, List, ListItem, Text } from 'native-base';
 import { Location } from 'expo';
 import axios from 'axios';
+import MapView, { Marker } from 'react-native-maps';
 
 const MechanicsScreen = () => {
   const [userLocation, setUserLocation] = useState(null);
@@ -29,12 +30,14 @@ const MechanicsScreen = () => {
 
   const fetchNearbyMechanics = async (coords) => {
     try {
-      const response = await axios.get(`https://wheelwellbackendservice.onrender.com/api/`, {
+      // Replace 'YOUR_BACKEND_API_URL' with the actual URL of your backend API
+      const response = await axios.get(`YOUR_BACKEND_API_URL/mechanics`, {
         params: {
           latitude: coords.latitude,
           longitude: coords.longitude,
         },
       });
+
       setNearbyMechanics(response.data);
     } catch (error) {
       console.error('Failed to fetch nearby mechanics:', error);
@@ -51,8 +54,34 @@ const MechanicsScreen = () => {
         <Right />
       </Header>
       <Content contentContainerStyle={{ flex: 1 }}>
-        <View>
-          {/* Implement your map to display userLocation and nearbyMechanics */}
+        <View style={{ flex: 1 }}>
+          {userLocation && (
+            <MapView
+              style={{ flex: 1 }}
+              region={{
+                latitude: userLocation.latitude,
+                longitude: userLocation.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+              }}
+            >
+              {/* User marker */}
+              <Marker coordinate={userLocation} title="Your Location" />
+
+              {/* Markers for nearby mechanics */}
+              {nearbyMechanics.map((mechanic) => (
+                <Marker
+                  key={mechanic._id}
+                  coordinate={{
+                    latitude: mechanic.latitude,
+                    longitude: mechanic.longitude,
+                  }}
+                  title={mechanic.name}
+                  description={`Contact: ${mechanic.contact}, Expertise: ${mechanic.expertise}`}
+                />
+              ))}
+            </MapView>
+          )}
         </View>
         <List>
           {nearbyMechanics.map((mechanic) => (
