df5a38b781a40acff50b9d1d4a79ac59c17e80ee
koomekevo <koome.kevo@gurucrafts.net>
2023-07-29 09:34:17 +0300

    Add mongoose and dotenv

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+// index.js
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// Routes
+const mechanicsRoutes = require('./routes/mechanics');
+app.use('/api', mechanicsRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
