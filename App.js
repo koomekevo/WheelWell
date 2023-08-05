2f478823dad0d7501f9fc204dddb45be5097ad3f
koomekevo <koome.kevo@gurucrafts.net>
2023-08-05 10:19:46 +0300

    Modify App.js with DriverSignUpScreen.js and MechanicSignUpScreen.js

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 // App.js
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-import AuthScreen from './screens/AuthScreen';
-import HomeScreen from './screens/HomeScreen';
-import MechanicsScreen from './screens/MechanicsScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import AuthScreen from "./screens/AuthScreen";
+import HomeScreen from "./screens/HomeScreen";
+import MechanicsScreen from "./screens/MechanicsScreen";
 
 const Tab = createBottomTabNavigator();
 
@@ -17,20 +17,20 @@ const App = () => {
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
-            if (route.name === 'Auth') {
-              iconName = focused ? 'log-in' : 'log-in-outline';
-            } else if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Mechanics') {
-              iconName = focused ? 'map' : 'map-outline';
+            if (route.name === "Auth") {
+              iconName = focused ? "log-in" : "log-in-outline";
+            } else if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Mechanics") {
+              iconName = focused ? "map" : "map-outline";
             }
 
             return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
         tabBarOptions={{
-          activeTintColor: 'blue',
-          inactiveTintColor: 'gray',
+          activeTintColor: "blue",
+          inactiveTintColor: "gray",
         }}
       >
         <Tab.Screen name="Auth" component={AuthScreen} />
