35ddc45a51f74f1975b0734a41a234662059b772
koomekevo <koome.kevo@gurucrafts.net>
2023-08-12 09:37:00 +0300

    Recreate WheelWell

--- App.js
+++ App.js
@@ -1,49 +1,20 @@
-import React, { useEffect } from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import MechanicScreen from "./screens/MechanicScreen";
-import DriverScreen from "./screens/DriverScreen";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { initializeApp } from "firebase/app";
-import styled from "styled-components/native";
-import Icon from "react-native-vector-icons/Ionicons";
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-const Container = styled.View`
-  flex: 1;
-  background-color: #fff;
-`;
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-const App = () => {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Mechanic"
-          component={MechanicScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="build" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Driver"
-          component={DriverScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="car" color={color} size={size} />
-            ),
-          }}
-        />
-        {/* Add other tab screens and navigation options */}
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
