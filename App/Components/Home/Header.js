0316bc9250a1923dce45df3289d2df33025fa840
koomekevo <koome.kevo@gurucrafts.net>
2023-08-14 14:47:27 +0300

    Modify Header.js

--- App/Components/Home/Header.js
+++ App/Components/Home/Header.js
@@ -1,57 +1,58 @@
-import { View, Text, Dimensions } from "react-native";
-import React, { useContext, useEffect, useState } from "react";
-import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
-import { UserLocationContext } from "../../Context/UserLocationContext";
-import PlaceMarker from "./PlaceMarker";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TextInput,
+  Dimensions,
+} from "react-native";
+import React from "react";
 import Colors from "../../Shared/Colors";
 
-export default function GoogleMapView({ placeList }) {
-  const [mapRegion, setmapRegion] = useState([]);
-
-  const { location, setLocation } = useContext(UserLocationContext);
-
-  useEffect(() => {
-    if (location) {
-      setmapRegion({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.0422,
-        longitudeDelta: 0.0421,
-      });
-    }
-  }, [location]);
-
+export default function Header() {
   return (
-    <View style={{ marginTop: 20 }}>
-      <Text
-        style={{
-          fontSize: 20,
-          marginBottom: 10,
-          fontWeight: "600",
-          fontFamily: "raleway-bold",
-        }}
-      >
-        Top Near By Places
-      </Text>
-      <View style={{ borderRadius: 20, overflow: "hidden" }}>
-        {location ? (
-          <MapView
-            style={{
-              width: Dimensions.get("screen").width * 0.89,
-              height: Dimensions.get("screen").height * 0.23,
-            }}
-            provider={PROVIDER_GOOGLE}
-            showsUserLocation={true}
-            region={mapRegion}
-          >
-            <Marker title="You" coordinate={mapRegion} />
-            {placeList.map(
-              (item, index) =>
-                index <= 4 && <PlaceMarker item={item} key={index} />
-            )}
-          </MapView>
-        ) : null}
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gap: 10,
+        alignItems: "center",
+      }}
+    >
+      <Image
+        source={require("./../../../assets/logo.png")}
+        style={styles.logo}
+      />
+
+      <View>
+        <TextInput placeholder="Search" style={styles.searchBar} />
       </View>
+      <Image
+        source={require("./../../../assets/user.png")}
+        style={styles.userImage}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 50,
+    height: 50,
+  },
+  searchBar: {
+    borderWidth: 1,
+    borderColor: Colors.GRAY,
+    padding: 4,
+    borderRadius: 50,
+    paddingLeft: 10,
+    width: Dimensions.get("screen").width * 0.53,
+    borderColor: Colors.PRIMARY,
+  },
+  userImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+  },
+});
