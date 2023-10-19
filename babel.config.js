6c88b7c8f9ed79fbdd6e6ec20037cc1a0f26c2b6
koomekevo <koome.kevo@gurucrafts.net>
2023-10-19 17:25:58 +0300

    Use styled.components instead of TailWindCSS

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
-// babel.config.js
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    plugins: ["nativewind/babel"],
+    presets: ['babel-preset-expo'],
   };
 };
