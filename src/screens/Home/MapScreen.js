89822c97bf1ca283a9fc9c7fc36473bf2dfe7305
koomekevo <koome.kevo@gurucrafts.net>
2023-10-23 22:18:28 +0300

    Modify MapScreen.js and RequestListScreen.js

--- src/screens/Home/MapScreen.js
+++ src/screens/Home/MapScreen.js
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MapViewContainer = styled.View`
+  width: 90%;
+  height: 300px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-bottom: 20px;
+`;
+
+const RefreshButton = styled.Button`
+  width: 90%;
+`;
 
 const MapScreen = () => {
-  return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
-  )
-}
+  const handleRefresh = () => {
+    // Add logic to refresh the map here
+  };
 
-export default MapScreen
+  return (
+    <Container>
+      <MapViewContainer>
+        {/* Add your map component here */}
+        <Text>Map View Goes Here</Text>
+      </MapViewContainer>
+      <RefreshButton title="Refresh Map" onPress={handleRefresh} />
+    </Container>
+  );
+};
+
+export default MapScreen;
