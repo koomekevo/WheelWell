37fc29e9bf12e98534cbef759b118e560c768818
koomekevo <koome.kevo@gurucrafts.net>
2023-10-23 20:45:59 +0300

    Modify BottomTabBar.js and UserProfile.js

--- src/components/Common/BottomTabBar.js
+++ src/components/Common/BottomTabBar.js
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
 
-const BottomTabBar = () => {
+const Container = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #fff;
+  height: 60px;
+  border-top-width: 1px;
+  border-top-color: #ccc;
+`;
+
+const TabItem = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const TabText = styled.Text`
+  font-size: 14px;
+`;
+
+const BottomTabBar = ({ tabs, activeTab, onTabPress }) => {
   return (
-    <View>
-      <Text>BottomTabBar</Text>
-    </View>
-  )
-}
+    <Container>
+      {tabs.map((tab, index) => (
+        <TabItem
+          key={index}
+          onPress={() => onTabPress(index)}
+          style={{
+            backgroundColor: activeTab === index ? "#007BFF" : "transparent",
+          }}
+        >
+          <TabText style={{ color: activeTab === index ? "#fff" : "#000" }}>
+            {tab.label}
+          </TabText>
+        </TabItem>
+      ))}
+    </Container>
+  );
+};
 
-export default BottomTabBar
+export default BottomTabBar;
