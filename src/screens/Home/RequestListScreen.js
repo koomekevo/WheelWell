db1d525981a5c014995f45c4402c408737bf1186
koomekevo <koome.kevo@gurucrafts.net>
2023-10-23 19:46:43 +0300

    Add files for frontend

---
+++ src/screens/Home/RequestListScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RequestListScreen = () => {
+  return (
+    <View>
+      <Text>RequestListScreen</Text>
+    </View>
+  )
+}
+
+export default RequestListScreen
