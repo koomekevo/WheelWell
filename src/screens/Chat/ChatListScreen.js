80369d9a26ec71292c3511db3a143815fe3e3b63
koomekevo <koome.kevo@gurucrafts.net>
2023-10-23 21:45:02 +0300

    Add ChatListScreen.js and ChatScreen.js

--- src/screens/Chat/ChatListScreen.js
+++ src/screens/Chat/ChatListScreen.js
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import styled from 'styled-components/native';
 
-const ChatListScreen = () => {
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
+
+const ChatItem = styled.TouchableOpacity`
+  padding: 20px;
+  border-bottom-width: 1px;
+  border-bottom-color: #ccc;
+`;
+
+const ChatTitle = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+const ChatPreview = styled.Text`
+  font-size: 14px;
+  color: #888;
+`;
+
+const ChatListScreen = ({ chats, onChatPress }) => {
   return (
-    <View>
-      <Text>ChatListScreen</Text>
-    </View>
-  )
-}
+    <Container>
+      <FlatList
+        data={chats}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <ChatItem onPress={() => onChatPress(item.id)}>
+            <ChatTitle>{item.title}</ChatTitle>
+            <ChatPreview>{item.lastMessage}</ChatPreview>
+          </ChatItem>
+        )}
+      />
+    </Container>
+  );
+};
 
-export default ChatListScreen
+export default ChatListScreen;
--- src/screens/Chat/ChatScreen.js
+++ src/screens/Chat/ChatScreen.js
@@ -1,12 +1,83 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList, TextInput, Button, KeyboardAvoidingView } from 'react-native';
+import styled from 'styled-components/native';
 
-const ChatScreen = () => {
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
+
+const ChatHeader = styled.View`
+  padding: 20px;
+  border-bottom-width: 1px;
+  border-bottom-color: #ccc;
+`;
+
+const ChatHeaderText = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const MessageList = styled.FlatList`
+  flex: 1;
+  padding: 10px;
+`;
+
+const MessageInputContainer = styled.View`
+  padding: 10px;
+  border-top-width: 1px;
+  border-top-color: #ccc;
+`;
+
+const MessageInput = styled.TextInput`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const SendButton = styled.Button`
+  width: 90%;
+  align-self: center;
+`;
+
+const ChatScreen = ({ chatTitle, messages, onSendMessage }) => {
+  const [message, setMessage] = useState('');
+
+  const handleSendMessage = () => {
+    if (message.trim() !== '') {
+      onSendMessage(message);
+      setMessage('');
+    }
+  };
+
+  useEffect(() => {
+    // Scroll to the end of the message list when new messages arrive
+    messageListRef.current.scrollToEnd();
+  }, [messages]);
+
   return (
-    <View>
-      <Text>ChatScreen</Text>
-    </View>
-  )
-}
+    <Container>
+      <ChatHeader>
+        <ChatHeaderText>{chatTitle}</ChatHeaderText>
+      </ChatHeader>
+      <MessageList
+        data={messages}
+        keyExtractor={(item, index) => `message-${index}`}
+        renderItem={({ item }) => (
+          <Text>{item.text}</Text>
+        )}
+      />
+      <MessageInputContainer>
+        <MessageInput
+          placeholder="Type a message..."
+          value={message}
+          onChangeText={(text) => setMessage(text)}
+        />
+        <SendButton title="Send" onPress={handleSendMessage} />
+      </MessageInputContainer>
+    </Container>
+  );
+};
 
-export default ChatScreen
+export default ChatScreen;
