e87dcbb1fdd0922f62de68b086d93c9d87b2b73f
koomekevo <koome.kevo@gurucrafts.net>
2023-10-23 22:12:15 +0300

    Add BottomTabNavigator.js and UserProfileScreen.js

--- src/screens/Common/BottomTabNavigator.js
+++ src/screens/Common/BottomTabNavigator.js
@@ -1,12 +1,79 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text } from "react-native";
+import styled from "styled-components/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Tab = createBottomTabNavigator();
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
+
+const ChatScreen = () => {
+  return (
+    <View>
+      <Text>Chat Screen</Text>
+    </View>
+  );
+};
+
+const RequestsScreen = () => {
+  return (
+    <View>
+      <Text>Requests Screen</Text>
+    </View>
+  );
+};
+
+const ProfileScreen = () => {
+  return (
+    <View>
+      <Text>Profile Screen</Text>
+    </View>
+  );
+};
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
+
+const TabLabel = styled.Text`
+  font-size: 18px;
+`;
 
 const BottomTabNavigator = () => {
   return (
-    <View>
-      <Text>BottomTabNavigator</Text>
-    </View>
-  )
-}
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ tabBarLabel: "Home" }}
+        />
+        <Tab.Screen
+          name="Chat"
+          component={ChatScreen}
+          options={{ tabBarLabel: "Chat" }}
+        />
+        <Tab.Screen
+          name="Requests"
+          component={RequestsScreen}
+          options={{ tabBarLabel: "Requests" }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ tabBarLabel: "Profile" }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default BottomTabNavigator
+export default BottomTabNavigator;
--- src/screens/Common/UserProfileScreen.js
+++ src/screens/Common/UserProfileScreen.js
@@ -1,12 +1,77 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, Button } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const Section = styled.View`
+  margin-bottom: 20px;
+`;
+
+const SectionTitle = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+const ProfileItem = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+`;
+
+const ProfileItemLabel = styled.Text`
+  font-size: 16px;
+`;
+
+const ProfileItemValue = styled.Text`
+  font-size: 16px;
+  color: #007bff;
+`;
 
 const UserProfileScreen = () => {
   return (
-    <View>
-      <Text>UserProfileScreen</Text>
-    </View>
-  )
-}
+    <Container>
+      <Title>User Profile</Title>
 
-export default UserProfileScreen
+      <Section>
+        <SectionTitle>Profile Settings</SectionTitle>
+        <ProfileItem>
+          <ProfileItemLabel>Name:</ProfileItemLabel>
+          <ProfileItemValue>John Doe</ProfileItemValue>
+        </ProfileItem>
+        <ProfileItem>
+          <ProfileItemLabel>Email:</ProfileItemLabel>
+          <ProfileItemValue>johndoe@example.com</ProfileItemValue>
+        </ProfileItem>
+        <ProfileItem>
+          <ProfileItemLabel>User Type:</ProfileItemLabel>
+          <ProfileItemValue>Motorist</ProfileItemValue>
+        </ProfileItem>
+      </Section>
+
+      <Button
+        title="Edit Profile"
+        onPress={() => {
+          /* Add logic to navigate to the edit profile screen */
+        }}
+      />
+    </Container>
+  );
+};
+
+export default UserProfileScreen;
