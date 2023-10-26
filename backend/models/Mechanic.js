05ae83e839d02bb3059abfdc63dace65a9462696
koomekevo <koome.kevo@gurucrafts.net>
2023-10-26 17:19:40 +0300

    Add Mechanic.js

---
+++ backend/models/Mechanic.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const mechanicSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  specialization: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  // Add more fields specific to mechanics here
+});
+
+const Mechanic = mongoose.model('Mechanic', mechanicSchema);
+
+module.exports = Mechanic;
