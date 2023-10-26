bf79771188fc58f86ec8f42f4abeb328726f3b29
koomekevo <koome.kevo@gurucrafts.net>
2023-10-26 18:06:52 +0300

    Add mechanicService.js

---
+++ backend/services/mechanicService.js
@@ -0,0 +1,14 @@
+// Import the Mechanic model and any other required dependencies
+const Mechanic = require("../models/Mechanic"); // Replace with your actual Mechanic model
+
+const mechanicService = {
+  getMechanicProfile: (req, res) => {
+    // Implement logic to retrieve mechanic profile
+  },
+  updateMechanicProfile: (req, res) => {
+    // Implement logic to update mechanic profile
+  },
+  // Add more mechanic-related service functions as needed
+};
+
+module.exports = mechanicService;
