01ee2bf2648dd69141afda46e507460cd705f405
koomekevo <koome.kevo@gurucrafts.net>
2023-10-28 16:54:02 +0300

    Add functionality to UserProfileScreen.js

--- src/screens/Common/UserProfileScreen.js
+++ src/screens/Common/UserProfileScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, Button } from "react-native";
 import styled from "styled-components/native";
+import { useNavigation } from "@react-navigation/native"; // Import the navigation hook
 
 const Container = styled.View`
   flex: 1;
@@ -44,6 +45,28 @@ const ProfileItemValue = styled.Text`
 `;
 
 const UserProfileScreen = () => {
+  const navigation = useNavigation(); // Initialize the navigation hook
+
+  const handleEditProfile = () => {
+    // Add logic to fetch user data from the backend
+    fetch("http://your-backend-url/api/user", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer your_jwt_token", // Replace with a valid JWT token
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Handle the response data (e.g., update the user's information)
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+      });
+    // Then navigate to the edit profile screen
+    navigation.navigate("EditProfile"); // Replace "EditProfile" with the actual name of your edit profile screen
+  };
+
   return (
     <Container>
       <Title>User Profile</Title>
@@ -64,12 +87,7 @@ const UserProfileScreen = () => {
         </ProfileItem>
       </Section>
 
-      <Button
-        title="Edit Profile"
-        onPress={() => {
-          /* Add logic to navigate to the edit profile screen */
-        }}
-      />
+      <Button title="Edit Profile" onPress={handleEditProfile} />
     </Container>
   );
 };
