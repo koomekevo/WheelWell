184c61d7c0370afbc9d908e04acd2220ce0ca10f
koomekevo <koome.kevo@gurucrafts.net>
2023-11-04 11:53:02 +0300

    Modify mechanicController.js

--- backend/controllers/mechanicController.js
+++ backend/controllers/mechanicController.js
@@ -1,43 +1,40 @@
-// Import the Mechanic model and any required dependencies
-const Mechanic = require("../models/Mechanic"); // Replace with your actual Mechanic model
+const Mechanic = require('../models/Mechanic'); // Import your Mechanic model
 
 const mechanicController = {
-  // Get mechanic profile
-  getMechanicProfile: (req, res) => {
-    res.json({ mechanic: req.user });
-  },
+  // Create a new mechanic profile
+  createMechanic: async (req, res) => {
+    const { userId, /* other mechanic data */ } = req.body;
 
-  // Update mechanic profile
-  updateMechanicProfile: (req, res) => {
-    const { username, email, fullName /* other mechanic fields */ } = req.body;
-    const mechanicId = req.user.id;
+    try {
+      // Check if a mechanic profile already exists for the user
+      const existingMechanic = await Mechanic.findOne({ user: userId });
 
-    // Find the mechanic by their ID and update the profile fields
-    Mechanic.findByIdAndUpdate(
-      mechanicId,
-      { username, email, fullName /* other mechanic fields */ },
-      { new: true }, // Return the updated mechanic
-      (err, updatedMechanic) => {
-        if (err) {
-          return res.status(500).json({ message: "Profile update failed" });
-        }
-
-        res.json({ mechanic: updatedMechanic });
-      }
-    );
-  },
-
-  // Add more mechanic-related actions as needed
-
-  // Example: Delete mechanic account
-  deleteMechanic: (req, res) => {
-    Mechanic.findByIdAndRemove(req.user.id, (err) => {
-      if (err) {
-        return res.status(500).json({ message: "Account deletion failed" });
+      if (existingMechanic) {
+        return res.status(400).json({ message: 'Mechanic profile already exists' });
       }
 
-      res.json({ message: "Account deleted successfully" });
-    });
+      // Create a new mechanic profile
+      const mechanic = new Mechanic({ user: userId, /* other mechanic data */ });
+
+      await mechanic.save();
+
+      return res.status(201).json({ message: 'Mechanic profile created successfully' });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Mechanic profile creation failed' });
+    }
+  },
+
+  // Get a list of available mechanics
+  getAvailableMechanics: async (req, res) => {
+    try {
+      const availableMechanics = await Mechanic.find({ /* Add conditions to filter available mechanics */ });
+
+      return res.status(200).json(availableMechanics);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Failed to fetch available mechanics' });
+    }
   },
 };
 
