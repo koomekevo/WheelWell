b8a5099310b3219d51c05901037a96efeadb768a
koomekevo <koome.kevo@gurucrafts.net>
2023-11-04 12:13:03 +0300

    Modify requestController.js

--- backend/controllers/requestController.js
+++ backend/controllers/requestController.js
@@ -1,102 +1,46 @@
-// Import the Request model and any required dependencies
-const Request = require("../models/Request"); // Replace with your actual Request model
+const Request = require("../models/Request"); // Import your Request model
 
 const requestController = {
-  // Create a new service request
-  createRequest: (req, res) => {
-    const { title, description, location /* other request fields */ } =
-      req.body;
+  // Send a request
+  sendRequest: async (req, res) => {
+    const { motoristId, mechanicId, message } = req.body;
 
-    // Create a new request
-    const newRequest = new Request({
-      title,
-      description,
-      location,
-      /* other request fields */
-      motorist: req.user.id, // Assuming the requester is a motorist
-    });
+    try {
+      // Create a new request
+      const request = new Request({
+        motorist: motoristId,
+        mechanic: mechanicId,
+        message,
+      });
 
-    newRequest.save((err, request) => {
-      if (err) {
-        return res.status(500).json({ message: "Request creation failed" });
-      }
+      await request.save();
 
-      res
-        .status(201)
-        .json({ message: "Request created successfully", request });
-    });
+      return res.status(201).json({ message: "Request sent successfully" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Request sending failed" });
+    }
   },
 
-  // Get all service requests
-  getAllRequests: (req, res) => {
-    Request.find({}, (err, requests) => {
-      if (err) {
-        return res.status(500).json({ message: "Error fetching requests" });
+  // Get requests sent to a mechanic or received by a motorist
+  getRequests: async (req, res) => {
+    const { userId, userType } = req.params;
+
+    try {
+      let requests = [];
+
+      if (userType === "mechanic") {
+        requests = await Request.find({ mechanic: userId });
+      } else if (userType === "motorist") {
+        requests = await Request.find({ motorist: userId });
       }
 
-      res.json({ requests });
-    });
+      return res.status(200).json(requests);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Failed to fetch requests" });
+    }
   },
-
-  // Get a specific service request by ID
-  getRequestById: (req, res) => {
-    const requestId = req.params.requestId;
-
-    Request.findById(requestId, (err, request) => {
-      if (err) {
-        return res.status(500).json({ message: "Error fetching request" });
-      }
-
-      if (!request) {
-        return res.status(404).json({ message: "Request not found" });
-      }
-
-      res.json({ request });
-    });
-  },
-
-  // Update a service request by ID
-  updateRequest: (req, res) => {
-    const requestId = req.params.requestId;
-    const { title, description, location /* other request fields */ } =
-      req.body;
-
-    Request.findByIdAndUpdate(
-      requestId,
-      {
-        title,
-        description,
-        location,
-        /* other request fields */
-      },
-      { new: true }, // Return the updated request
-      (err, updatedRequest) => {
-        if (err) {
-          return res.status(500).json({ message: "Request update failed" });
-        }
-
-        res.json({
-          message: "Request updated successfully",
-          request: updatedRequest,
-        });
-      }
-    );
-  },
-
-  // Delete a service request by ID
-  deleteRequest: (req, res) => {
-    const requestId = req.params.requestId;
-
-    Request.findByIdAndRemove(requestId, (err) => {
-      if (err) {
-        return res.status(500).json({ message: "Request deletion failed" });
-      }
-
-      res.json({ message: "Request deleted successfully" });
-    });
-  },
-
-  // Add more request-related actions as needed
 };
 
 module.exports = requestController;
