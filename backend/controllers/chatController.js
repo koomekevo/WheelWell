b14d52e31e570378e43bcfe6ab49e96313c366e4
koomekevo <koome.kevo@gurucrafts.net>
2023-11-04 11:03:34 +0300

    Modify controllers/chatController.js

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,55 +1,53 @@
-// Import the Chat model and any required dependencies
-const Chat = require("../models/Chat"); // Replace with your actual Chat model
+const Chat = require("../models/Chat"); // Import your Chat model
+const User = require("../models/User"); // Import your User model
 
 const chatController = {
-  // Get chat messages for a specific chat room
-  getChatMessages: (req, res) => {
-    const chatId = req.params.chatId;
+  // Create a new chat
+  createChat: async (req, res) => {
+    const { senderId, receiverId, message } = req.body;
 
-    Chat.findById(chatId, (err, chat) => {
-      if (err) {
+    try {
+      // Check if the sender and receiver exist
+      const sender = await User.findById(senderId);
+      const receiver = await User.findById(receiverId);
+
+      if (!sender || !receiver) {
         return res
-          .status(500)
-          .json({ message: "Error fetching chat messages" });
+          .status(400)
+          .json({ message: "Sender or receiver not found" });
       }
 
-      if (!chat) {
-        return res.status(404).json({ message: "Chat not found" });
-      }
-
-      res.json({ messages: chat.messages });
-    });
-  },
-
-  // Send a new message in a chat room
-  sendMessage: (req, res) => {
-    const chatId = req.params.chatId;
-    const { text, sender } = req.body;
-
-    Chat.findById(chatId, (err, chat) => {
-      if (err) {
-        return res.status(500).json({ message: "Error sending message" });
-      }
-
-      if (!chat) {
-        return res.status(404).json({ message: "Chat not found" });
-      }
-
-      // Create a new message and add it to the chat
-      chat.messages.push({ text, sender });
-
-      // Save the updated chat
-      chat.save((saveErr) => {
-        if (saveErr) {
-          return res.status(500).json({ message: "Error sending message" });
-        }
-
-        res.json({ message: "Message sent successfully" });
+      // Create a new chat
+      const chat = new Chat({
+        sender: senderId,
+        receiver: receiverId,
+        messages: [{ user: senderId, text: message }],
       });
-    });
+
+      await chat.save();
+
+      return res.status(201).json({ message: "Chat created successfully" });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Chat creation failed" });
+    }
   },
 
-  // Add more chat-related actions as needed
+  // Get all chats for a user
+  getChats: async (req, res) => {
+    const userId = req.params.userId;
+
+    try {
+      const chats = await Chat.find({
+        $or: [{ sender: userId }, { receiver: userId }],
+      }).populate("sender receiver");
+
+      return res.status(200).json(chats);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Failed to fetch chats" });
+    }
+  },
 };
 
 module.exports = chatController;
