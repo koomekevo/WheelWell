39541171ab70b48227bd2f6bc5c1df6950ee6aa2
koomekevo <koome.kevo@gurucrafts.net>
2023-11-04 12:56:45 +0300

    Modify models/User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
   },
   email: {
     type: String,
@@ -14,12 +16,36 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  userType: {
+  role: {
     type: String,
-    enum: ["Motorist", "Mechanic"],
+    enum: ["motorist", "mechanic"],
     required: true,
   },
 });
+
+// Hash the user's password before saving to the database
+userSchema.pre("save", async function (next) {
+  try {
+    if (!this.isModified("password")) {
+      return next();
+    }
+
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// Method to compare passwords during login
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (err) {
+    throw err;
+  }
+};
 
 const User = mongoose.model("User", userSchema);
 
