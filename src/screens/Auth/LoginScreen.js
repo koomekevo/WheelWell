0e88d098f8847ad1495390da66c5536024d55f4e
koomekevo <koome.kevo@gurucrafts.net>
2023-11-17 09:26:59 +0300

    Resetup WheelWell dev environment

--- src/screens/Auth/LoginScreen.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Login from "../../components/Auth/Login";
-
-const LoginScreen = ({ navigation }) => {
-  return <Login navigation={navigation} />;
-};
-
-export default LoginScreen;
--- src/screens/Auth/RegisterScreen.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Register from "../../components/Auth/Register";
-
-const RegisterScreen = ({ navigation }) => {
-  return <Register navigation={navigation} />;
-};
-
-export default RegisterScreen;
