cdceb03b15366acab115591af5a656527a7867cf
koomekevo <koome.kevo@gurucrafts.net>
2024-01-25 16:52:24 +0300

    Add app/(tabs)/inbox.tsx, profile.tsx and wishlists.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,9 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
 
 const _layout = () => {
-  return (
-    <View>
-      <Text>_layout</Text>
-    </View>
-  )
-}
+  return <Tabs></Tabs>;
+};
 
-export default _layout
+export default _layout;
