7e4e8f79cb894f9856889adebe50c479b41069b6
koomekevo <koome.kevo@gurucrafts.net>
2024-01-25 17:42:51 +0300

    Add Explore icon in app/(tabs)/layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,30 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import Colors from "@/constants/Colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-const _layout = () => {
-  return <Tabs></Tabs>;
+const Layout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors.primary,
+        tabBarLabelStyle: {
+          fontFamily: "mon-sb",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Explore",
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="search" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 
-export default _layout;
+export default Layout;
