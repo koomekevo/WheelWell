ac3bbd301cd74ff94dc32407238b7281c629b96c
koomekevo <koome.kevo@gurucrafts.net>
2024-01-25 18:01:12 +0300

    Add Wishlists, Trips, Inbox and Profile icons in app/(tabs)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Tabs } from "expo-router";
 import Colors from "@/constants/Colors";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { FontAwesome5 } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 
 const Layout = () => {
   return (
@@ -19,7 +18,43 @@ const Layout = () => {
         options={{
           tabBarLabel: "Explore",
           tabBarIcon: ({ color, size }) => (
-            <FontAwesome5 name="search" color={color} size={size} />
+            <Ionicons name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="wishlists"
+        options={{
+          tabBarLabel: "Wishlists",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart-outline" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="trips"
+        options={{
+          tabBarLabel: "Trips",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="car" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="inbox"
+        options={{
+          tabBarLabel: "Inbox",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="mail" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-circle-outline" color={color} size={size} />
           ),
         }}
       />
