5e116d766890bf7f8c4cc35e705a9e960bbc0b6c
koomekevo <koome.kevo@gurucrafts.net>
2024-02-01 00:06:10 +0300

    Add Platform.OS paddingTop in ExploreHeader.tsx

--- components/ExploreHeader.tsx
+++ components/ExploreHeader.tsx
@@ -1,7 +1,15 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Platform,
+} from "react-native";
 import React from "react";
 import { Ionicons } from "@expo/vector-icons";
 import { MaterialIcons } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 const categories = [
   {
@@ -36,10 +44,30 @@ const categories = [
 
 const ExploreHeader = () => {
   return (
-    <View>
-      <Text>ExploreHeader</Text>
-    </View>
+    <SafeAreaView
+      style={{ flex: 1, paddingTop: Platform.OS === "android" ? 40 : 0 }}
+    >
+      <View>
+        <View style={styles.container}>
+          <View styles={styles.actionRow}>
+            <Link href={"/(modals)/booking"}>Booking</Link>
+            <TouchableOpacity>
+              <Text>Filter</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+  },
+  actionRow: {
+    flexDirection: "row",
+  },
+});
 
 export default ExploreHeader;
