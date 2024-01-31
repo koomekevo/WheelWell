07f7635ee59b7dadba513257ba22d304b588163d
koomekevo <koome.kevo@gurucrafts.net>
2024-02-01 02:53:21 +0300

    Update components/Listings

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -100,7 +100,10 @@ function RootLayoutNav() {
           ),
         }}
       />
-      <Stack.Screen name="listing/[id]" options={{ headerTitle: "" }} />
+      <Stack.Screen
+        name="listing/[id]"
+        options={{ headerTitle: "", headerTransparent: true }}
+      />
       <Stack.Screen
         name="(modals)/booking"
         options={{
